869d54a474b8e85be5422eb817823f846e8ae860
Vincenzo Ingrao Jr <vincenzoingraojr@gmail.com>
2023-02-28 22:12:07 +0100

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,23 @@
 import Head from "../components/Head";
+import { useMeQuery } from "../generated/graphql";
 
 function HomePage() {
+    const { data } = useMeQuery({ fetchPolicy: "network-only" });
+
     return (
         <>
             <Head
                 title="Home | Square"
                 description="Do everything on this app."
             />
-            <>Homepage</>
+            <>
+                Welcome, {data?.me?.firstName} <br />
+                {data?.me?.sessions?.map((session) => (
+                    <div key={session.id}>
+                        {session.sessionId} {session.clientOS} {session.clientName} {session.deviceLocation} {session.creationDate}
+                    </div>
+                ))}
+            </>
         </>
     );
 }
